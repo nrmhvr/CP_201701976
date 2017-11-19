8f60eb07aede89a1e8391bce4072ec6113126916
nrmhvr <123dusdn@naver.com>
2017-11-19 22:11:14 +0900

    second assignment

---
+++ second assignment/Main2_3_2.java
@@ -0,0 +1,32 @@
+
+public class Main2_3_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//방법2
+		int result;
+		result = 1*1;
+		System.out.println("1부터 1까지의 제곱의 합은 " +result+ " 입니다.");
+		result = result + 2*2;
+		System.out.println("1부터 2까지의 제곱의 합은 " +result+ " 입니다.");
+		result = result + 3*3;
+		System.out.println("1부터 3까지의 제곱의 합은 " +result+ " 입니다.");
+		result = result + 4*4;
+		System.out.println("1부터 4까지의 제곱의 합은 " +result+ " 입니다.");
+		result = result + 5*5;
+		System.out.println("1부터 5까지의 제곱의 합은 " +result+ " 입니다.");
+		result = result + 6*6;
+		System.out.println("1부터 6까지의 제곱의 합은 " +result+ " 입니다.");
+		result = result + 7*7;
+		System.out.println("1부터 7까지의 제곱의 합은 " +result+ " 입니다.");		
+		result = result + 8*8;
+		System.out.println("1부터 8까지의 제곱의 합은 " +result+ " 입니다.");		
+		result = result + 9*9;
+		System.out.println("1부터 9까지의 제곱의 합은 " +result+ " 입니다.");		
+		result = result + 10*10;
+		System.out.println("1부터 10까지의 제곱의 합은 " +result+ " 입니다.");
+
+
+	}
+
+}
